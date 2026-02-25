f66659129be75a3cc081486732bde7998e4d1852
fl720 <99543089+fl720@users.noreply.github.com>
2026-02-25 23:56:20 +0000

    Add files via upload

--- lab_05/HPP.java
+++ lab_05/HPP.java
@@ -19,17 +19,21 @@ public class HPP {
 
     public static void main(String args []) throws Exception {
 
+        // initialize - staged head on collision in x direction.
+        fin [NX/2+1] [NY/2] [0] = true;
+        fin [NX/2-1] [NY/2] [1] = true;
+
         // initialize - populate a subblock of grid
-        for(int i = 0; i < NX/4 ; i++) { 
-            for(int j = 0; j < NY/4 ; j++) { 
-                boolean [] fin_ij = fin [i] [j] ;
-                for(int d = 0 ; d < q ; d++) {
-                    if(Math.random() < DENSITY) {
-                        fin_ij [d] = true ;
-                    }
-                }
-            }
-        }
+        // for(int i = 0; i < NX/4 ; i++) { 
+        //     for(int j = 0; j < NY/4 ; j++) { 
+        //         boolean [] fin_ij = fin [i] [j] ;
+        //         for(int d = 0 ; d < q ; d++) {
+        //             if(Math.random() < DENSITY) {
+        //                 fin_ij [d] = true ;
+        //             }
+        //         }
+        //     }
+        // }
 
 
         display.repaint() ;
@@ -46,12 +50,34 @@ public class HPP {
 
                     // default, no collisions case:
 
-                    fout_ij [0] = fin_ij [0] ;
-                    fout_ij [1] = fin_ij [1] ;
-                    fout_ij [2] = fin_ij [2] ;
-                    fout_ij [3] = fin_ij [3] ;
+                    // fout_ij [0] = fin_ij [0] ;
+                    // fout_ij [1] = fin_ij [1] ;
+                    // fout_ij [2] = fin_ij [2] ;
+                    // fout_ij [3] = fin_ij [3] ;
 
                     // please add collisions as per lecture!
+                    // please add collisions as per lecture!
+                    // 检查是否只有状态 0 和 1 (水平对撞)
+                    if (fin_ij[0] && fin_ij[1] && !fin_ij[2] && !fin_ij[3]) {
+                        fout_ij[0] = false;
+                        fout_ij[1] = false;
+                        fout_ij[2] = true;
+                        fout_ij[3] = true;
+                    } 
+                    // 检查是否只有状态 2 和 3 (垂直对撞)
+                    else if (!fin_ij[0] && !fin_ij[1] && fin_ij[2] && fin_ij[3]) {
+                        fout_ij[0] = true;
+                        fout_ij[1] = true;
+                        fout_ij[2] = false;
+                        fout_ij[3] = false;
+                    } 
+                    // 其他所有情况：没有碰撞发生，直接继承状态
+                    else {
+                        fout_ij[0] = fin_ij[0];
+                        fout_ij[1] = fin_ij[1];
+                        fout_ij[2] = fin_ij[2];
+                        fout_ij[3] = fin_ij[3];
+                    }
                 }
             }
 
@@ -65,31 +91,43 @@ public class HPP {
                     int jM1 = (j - 1 + NY) % NY ;
 
                     // no streaming case:
-                    if ( i != NX-1 ){
-                        fin [i] [j] [0] = fout [i+1] [j] [0] ;
-                    } else {
-                        fin [i] [j] [0] = fout [NX-1] [j] [1] ;
-                    }
+                    // if ( i != NX-1 ){
+                    //     fin [i] [j] [0] = fout [i+1] [j] [0] ;
+                    // } else {
+                    //     fin [i] [j] [0] = fout [NX-1] [j] [1] ;
+                    // }
 
-                    if ( i != 0 ){
-                        fin [i] [j] [1] = fout [i-1] [j] [1] ;
-                    } else {
-                        fin [i] [j] [1] = fout [0] [j] [0] ;
-                    }
+                    // if ( i != 0 ){
+                    //     fin [i] [j] [1] = fout [i-1] [j] [1] ;
+                    // } else {
+                    //     fin [i] [j] [1] = fout [0] [j] [0] ;
+                    // }
 
-                    if ( j != NY-1 ){
-                    fin [i] [j] [2] = fout [i] [j+1] [2] ;
-                    } else {
-                        fin [i] [j] [2] = fout [i] [NY-1] [3] ;
-                    }
+                    // if ( j != NY-1 ){
+                    // fin [i] [j] [2] = fout [i] [j+1] [2] ;
+                    // } else {
+                    //     fin [i] [j] [2] = fout [i] [NY-1] [3] ;
+                    // }
 
-                    if ( j != 0 ){
-                    fin [i] [j] [3] = fout [i] [j-1] [3] ;
-                    } else {
-                        fin [i] [j] [3] = fout [i] [0] [2] ;
-                    }
+                    // if ( j != 0 ){
+                    // fin [i] [j] [3] = fout [i] [j-1] [3] ;
+                    // } else {
+                    //     fin [i] [j] [3] = fout [i] [0] [2] ;
+                    // }
 
                     // please add streaming as per lecture!
+                    // please add streaming as per lecture!
+                    // 状态 0：向左 (-x) 移动，所以当前网格接收右边 (x+1) 的粒子
+                    fin [i] [j] [0] = fout [iP1] [j] [0] ;
+                    
+                    // 状态 1：向右 (+x) 移动，所以当前网格接收左边 (x-1) 的粒子
+                    fin [i] [j] [1] = fout [iM1] [j] [1] ;
+                    
+                    // 状态 2：向上 (-y) 移动，所以当前网格接收下边 (y+1) 的粒子
+                    fin [i] [j] [2] = fout [i] [jP1] [2] ;
+                    
+                    // 状态 3：向下 (+y) 移动，所以当前网格接收上边 (y-1) 的粒子
+                    fin [i] [j] [3] = fout [i] [jM1] [3] ;
                 }
             }
 
